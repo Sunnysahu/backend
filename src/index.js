0015a17b8d1f1fa30c00593206aebd91dd2676b5
Sunny Sahu <sunnysahu8210@gmail.com>
2024-04-25 17:38:16 +0530

    Connection Established

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
-import express from "express";
-
-import mongoose from "mongoose";
-
-import { DB_NAME } from "./constants.js";
-
 import "dotenv/config";
+import connectDB from "./db/index.js";
 
-const app = express();
+connectDB();
 
-(async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    app.on("Error", (error) => {
-      console.log("ERROR : ", error);
-      throw error;
-    });
-    app.listen(process.env.PORT, () => {
-      console.log(`App is listening at ${process.env.PORT}`);
-    });
-  } catch (error) {
-    console.error("EROR :", error);
-    throw error;
-  }
-})();
+// import express from "express";
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("Error", (error) => {
+//       console.log("ERROR : ", error);
+//       throw error;
+//     });
+//     app.listen(process.env.PORT, () => {
+//       console.log(`App is listening at ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.error("EROR :", error);
+//     throw error;
+//   }
+// })();
